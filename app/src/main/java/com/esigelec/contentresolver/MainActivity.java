d495876e4ffe4f833de785675bec091d584c4b66
ENZO <enzo.zyh@gmail.com>
2016-03-15 22:43:27 +0100

    Initial commit

---
+++ app/src/main/java/com/esigelec/contentresolver/MainActivity.java
@@ -0,0 +1,120 @@
+package com.esigelec.contentresolver;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    private ContentResolver contentResolver;
+    private Button btn_get_user, btn_get_user_name;
+    private EditText editxt_id;
+    private TextView txt_content;
+    private static final String TAG = "tag";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contentResolver = getContentResolver();
+
+        editxt_id = (EditText) findViewById(R.id.editxt_id);
+        txt_content = (TextView) findViewById(R.id.txt_content);
+        btn_get_user = (Button) findViewById(R.id.btn_get_user);
+        btn_get_user_name = (Button) findViewById(R.id.btn_get_user_name);
+
+        btn_get_user.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int id = Integer.valueOf(editxt_id.getText().toString());
+//                String[] projection = {UserContract.Entry._ID, UserContract.Entry.COLUMN_NAME_USER_NAME, UserContract
+//                        .Entry
+//                        .COLUMN_NAME_ACCOUNT, UserContract.Entry.COLUMN_NAME_PASSWORD, UserContract.Entry
+//                        .COLUMN_NAME_EMAIL, UserContract.Entry.COLUMN_NAME_ADDRESS};
+//                String selection = UserContract.Entry._ID + " LIKE ?";
+//                String[] selectionArgs = {String.valueOf(id)};
+                Uri uri = Uri.parse(UserContract.Entry.SCHEMA + UserContract.Entry.AUTHORITY + "/user/" + id);
+                Log.i(TAG, "--URI-->"+uri.toString());
+                Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+                if (cursor.getCount() <= 0)
+                {
+                    txt_content.setText("Cannot get the user!");
+                    return;
+                }
+
+                cursor.moveToFirst();
+                // only get the first one
+                int num_col_id = cursor.getColumnIndex(UserContract.Entry._ID);
+                int num_col_name = cursor.getColumnIndex(UserContract.Entry.COLUMN_NAME_USER_NAME);
+                int num_col_email = cursor.getColumnIndex(UserContract.Entry.COLUMN_NAME_EMAIL);
+                int num_col_address = cursor.getColumnIndex(UserContract.Entry.COLUMN_NAME_ADDRESS);
+                int num_col_account = cursor.getColumnIndex(UserContract.Entry.COLUMN_NAME_ACCOUNT);
+                int num_col_password = cursor.getColumnIndex(UserContract.Entry.COLUMN_NAME_PASSWORD);
+
+                String user_id = cursor.getString(num_col_id);
+                String user_name = cursor.getString(num_col_name);
+                String email = cursor.getString(num_col_email);
+                String address = cursor.getString(num_col_address);
+                String account = cursor.getString(num_col_account);
+                String password = cursor.getString(num_col_password);
+
+                StringBuilder sb = new StringBuilder();
+                sb.append(user_name);
+                sb.append(user_id);
+                sb.append(email);
+                sb.append(address);
+                sb.append(account);
+                sb.append(password);
+                txt_content.setText(sb.toString());
+            }
+        });
+
+        btn_get_user_name.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int id = Integer.valueOf(editxt_id.getText().toString());
+//                String[] projection = {UserContract.Entry._ID, UserContract.Entry.COLUMN_NAME_USER_NAME, UserContract
+//                        .Entry
+//                        .COLUMN_NAME_ACCOUNT, UserContract.Entry.COLUMN_NAME_PASSWORD, UserContract.Entry
+//                        .COLUMN_NAME_EMAIL, UserContract.Entry.COLUMN_NAME_ADDRESS};
+//                String selection = UserContract.Entry._ID + " LIKE ?";
+//                String[] selectionArgs = {String.valueOf(id)};
+                Uri uri = Uri.parse(UserContract.Entry.SCHEMA + UserContract.Entry.AUTHORITY + "/user/" + id + "/name");
+                Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+                if (cursor.getCount() <= 0)
+                {
+                    txt_content.setText("Cannot get the user!");
+                    return;
+                }
+
+                cursor.moveToFirst();
+                // only get the first one
+                int num_col_name = cursor.getColumnIndex(UserContract.Entry.COLUMN_NAME_USER_NAME);
+
+                String user_name = cursor.getString(num_col_name);
+
+                StringBuilder sb = new StringBuilder();
+                sb.append(user_name);
+
+                txt_content.setText(sb.toString());
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/esigelec/contentresolver/UserContract.java
@@ -0,0 +1,50 @@
+package com.esigelec.contentresolver;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by enzoz on 2016/3/11.
+ */
+public final class UserContract
+{
+    public UserContract()
+    {
+        // To prevent someone from accidentally instantiating the contract class,
+        // give it an empty constructor.
+    }
+
+    public static abstract class Entry implements BaseColumns
+    {
+        public static final String TABLE_NAME = "User";
+        //public static final String COLUMN_NAME_USER_ID = "user_id";
+        public static final String COLUMN_NAME_USER_NAME = "user_name";
+        public static final String COLUMN_NAME_ADDRESS = "address";
+        public static final String COLUMN_NAME_EMAIL = "email";
+        public static final String COLUMN_NAME_ACCOUNT = "account";
+        public static final String COLUMN_NAME_PASSWORD = "password";
+
+        // Authority
+        public static final String AUTHORITY = "com.example.enzo.practicedemos.DB.Providers.UserProvider";
+
+        // Path
+        public static final String PAHT_ALL_USER = "user";
+        public static final String PAHT_USER = "user/#";
+        public static final String PAHT_USER_NAME = "user/#/name";
+        public static final String PAHT_USER_ADDRESS = "user/#/address";
+        public static final String PAHT_USER_EMAIL = "user/#/email";
+        public static final String PAHT_USER_ACCOUNT = "user/#/account";
+        public static final String PAHT_USER_PASSWORD = "user/#/password";
+
+        // Uri
+        public static final String SCHEMA = "content://";
+        public static final Uri URI_ALL_USER = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_ALL_USER);
+        public static final Uri URI_USER = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_USER);
+        public static final Uri URI_USER_NAME = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_USER_NAME);
+        public static final Uri URI_USER_ADDRESS = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_USER_ADDRESS);
+        public static final Uri URI_USER_EMAIL = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_USER_EMAIL);
+        public static final Uri URI_USER_ACCOUNT = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_USER_ACCOUNT);
+        public static final Uri URI_USER_PASSWORD = Uri.parse(SCHEMA + AUTHORITY + "/" + PAHT_USER_PASSWORD);
+
+    }
+}
